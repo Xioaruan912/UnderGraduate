1e6084b019f8fd02899edf280f0bb0e156a0e527
Xioaruan912 <xioaruan@gmail.com>
2025-09-27 19:49:36 +0800

    Git

---
+++ 408/01_数据结构/代码/01.c
@@ -0,0 +1,41 @@
+// 在第k个位置插入元素i
+
+// 【分析】
+//  1.用户传入 i和 k 先要进行比对k的合法性 如果合法 那么直接插入即可
+//  2.顺序表在插入的时候 要求后面的内容先向后移动 移动完毕后插入数据
+
+#include <stdio.h>
+#define MAXSIZE 100
+typedef int Elemtype;
+
+typedef struct Sqlist
+{
+    Elemtype data[MAXSIZE];
+    int length;
+} Sqlist;
+
+struct Sqlist init_SQ()
+{
+    Sqlist Q;
+    int num = 999;
+    for (int i = 0; i <= 10; i++)
+    {
+        Q.data[i] = 999 + i;
+        Q.length++;
+    }
+    return Q;
+}
+
+void print_Q(Sqlist Q)
+{
+    for (int i = 0; i <= Q.length; i++)
+    {
+        printf("No.%d is %d", i, Q.data[i]);
+    }
+}
+
+int main()
+{
+    Sqlist Q = init_SQ();
+    print_Q(Q)
+}
